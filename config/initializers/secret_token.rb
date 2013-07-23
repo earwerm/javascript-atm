79b0110d187345c12d766f09591426b057f87663
earwerm <michael@earwerm.com>
2013-07-23 15:26:07 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Atm::Application.config.secret_key_base = '696514fa2103487f6056828776716fdacf67d2c5b3474793a7121fc4cca38b03e046a962a978555c20e0818325c794fd206810a72ebf60012dbc2b3d4bea4d23'
